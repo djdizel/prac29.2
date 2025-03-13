ee3c908c6e62de4302a01aa8bc560d0e756a569a
Filin Tir <filin2009@gmail.com>
2025-03-13 14:23:02 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prac29._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+        }
+    }
+}
