bb03c280d6a2cc767d97e10f4e6b8ec185c328c2
Filin Tir <filin2009@gmail.com>
2025-03-13 14:35:00 +0200

    prac29.2 need end

---
+++ printed .cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prac29._2
+{
+    internal class Printed
+    {
+        int copies;
+        string name;
+        int price;
+
+        public int Copies { get => copies; set => copies = value; }
+        public string Name { get => name; set => name = value; }
+        public int Price { get => price; set => price = value; }
+
+        public Printed() { }
+
+        public Printed(int copies, string name, int price)
+        {
+            Copies = copies;
+            Name = name;
+            Price = price;
+        }
+
+        public virtual int Cost()
+        {
+            return Copies * Price;
+        }
+        /// <summary>
+        /// Вывод
+        /// </summary>
+        public virtual void Output()
+        {
+            Console.WriteLine($"Название:{Name} Колво копий:{Copies} Цена:{Price}");
+        }
+    }
+}
